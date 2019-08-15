63f66ed0d1a650d832592ca7e3c7de9889ab0da9
CptJack <844501812@qq.com>
2019-08-15 10:55:02 +0800

    prettify code

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,10 @@
 
 using namespace std;
 
-unsigned long Sharding_Num=2;//120;
-int TopK=100;
-string file_name="../data/Dataset.txt";
+const int core_num=4;
+const unsigned long Sharding_Num=2;//120;
+const int TopK=100;
+const string file_name="../data/Dataset.txt";
 unsigned long file_len=0;
 
 void thread_func(unsigned long shard_index){
@@ -36,6 +37,7 @@ void map_files(){
     vector<ofstream> ofss(Sharding_Num);
     for(int i=0;i<Sharding_Num;++i)
         ofss[i].open("../data/shard_"+to_string(i)+".txt",ios::out);
+
     hash<string> hasher;
     string str;
     while(!ifs.eof()){
@@ -44,6 +46,8 @@ void map_files(){
         if(str.empty())continue;
         ofss[hasher(str)%Sharding_Num]<<str<<endl;
     }
+
+    ifs.close();
     for(auto& ele:ofss)
         ele.close();
 }
@@ -53,15 +57,16 @@ void reduce_files(){
         ifs.open("../data/shard_"+to_string(i)+".txt",ios::in);
         zset myzset;
         string str;
-        int total=0;
+
         while(!ifs.eof()){
             //stupid fucking fstream remain the string the way it was in last cycle when it read a blank line, fuck
             str.clear();
             ifs>>str;
             if(str.empty())continue;
             myzset.create_or_inc(str);
-            ++total;
         }
+        ifs.close();
+
         auto vec=myzset.pop(TopK);
         fstream ofs("../data/res_"+to_string(i)+".txt",ios::out);
         for(auto ele:vec)
@@ -91,6 +96,7 @@ void merge(){
         }
         ifs.close();
     }
+    
     ofstream ofs("../data/final.txt",ios::out);
     for(auto& ele:mylist)
         ofs<<ele.first<<" "<<ele.second<<endl;
@@ -103,8 +109,8 @@ int main(){
     fs.seekg(0, ios_base::end);
     file_len = fs.tellg();
     fs.close();
-//    map_files();
+    map_files();
 //    reduce_files();
-    merge();
+  //  merge();
     return 0;
 }
