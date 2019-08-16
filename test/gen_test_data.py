387edef799ef501fc89ea7c05229996c521d2361
CptJack <844501812@qq.com>
2019-08-16 14:16:12 +0800

    test code enhanced

--- test/gen_test_data.py
+++ test/gen_test_data.py
@@ -25,9 +25,21 @@ except Exception as e:
     print(e)
 indexs=numpy.random.randint(0, len(url_vec),distinct_urls_needed)
 nums = numpy.random.normal(mu, sigma,distinct_urls_needed)
+class url_t:
+    url=""
+    num=0
+    def __init__(self,u,n):
+        self.url=u
+        self.num=n
+    def __lt__(self, other):
+        return self.num>other.num
+ans_vec=[]
 for i in range(distinct_urls_needed):
     for j in range(int(nums[i])):
         f.write(url_vec[indexs[i]]+'\n')
-    fa.write(url_vec[indexs[i]]+" "+str(int(nums[i]))+'\n')
+    ans_vec.append(url_t(url_vec[indexs[i]],int(nums[i])))
+ans_vec.sort()
+for ele in ans_vec:
+    fa.write(ele.url+" "+str(ele.num)+'\n')
 f.close()
 fa.close()
