08bc03fe4eb8c7877b0f8b00e7bd73def3285ab3
CptJack <844501812@qq.com>
2019-08-20 10:41:06 +0800

    add test code

--- test/gen_test_data.py
+++ test/gen_test_data.py
@@ -4,31 +4,9 @@ urls_needed=1000
 #normal distribution param
 mu=500
 sigma=100
-#read in urls
-try:
-    f=open('../test/urls.txt','r')
-except Exception as e:
-    print(e)
-url_vec=[]
-for line in f:
-    line=line.strip()
-    url_vec.append(line)
-f.close()
-#gen urls subject to normal distribution
-try:
-    f=open('../data/Data.txt','w')
-except Exception as e:
-    print(e)
-try:
-    fa=open('../data/ans.txt','w')
-except Exception as e:
-    print(e)
-indexs=numpy.random.randint(0, len(url_vec), urls_needed)
-ind_set=set()
-for i in indexs:
-    ind_set.add(i)
-indexs=list(ind_set)
-nums = numpy.random.normal(mu, sigma,len(indexs))
+
+global url_vec
+
 class url_t:
     url=""
     num=0
@@ -37,14 +15,71 @@ class url_t:
         self.num=n
     def __lt__(self, other):
         return self.num>other.num
-ans_vec=[]
-for i in range(len(indexs)):
-    num=int(nums[i])
-    ans_vec.append(url_t(url_vec[indexs[i]],num))
-    for j in range(num):
-        f.write(url_vec[indexs[i]]+'\n')
-ans_vec.sort()
-for ele in ans_vec:
-    fa.write(ele.url+" "+str(ele.num)+'\n')
-f.close()
-fa.close()
+
+def gen_from_ans():
+    try:
+        f=open('../data/ans.txt','r')
+    except Exception as e:
+        print(e)
+    try:
+        fo=open('../data/Data.txt','w')
+    except Exception as e:
+        print(e)
+    urls=[]
+    nums=[]
+    for line in f:
+        t=line.strip().split(' ')
+        urls.append(t[0])
+        nums.append(int(t[1]))
+    for i,url in enumerate(urls):
+        for j in range(nums[i]):
+            fo.write(url+'\n')
+
+    f.close()
+    fo.close()
+
+def gen_normal_distributed(mu,sigma, urls_needed):
+    try:
+        f=open('../data/Data.txt','w')
+    except Exception as e:
+        print(e)
+    try:
+        fa=open('../data/ans.txt','w')
+    except Exception as e:
+        print(e)
+
+    indexs=numpy.random.randint(0, len(url_vec), urls_needed)
+    ind_set=set()
+    for i in indexs:
+        ind_set.add(i)
+    indexs=list(ind_set)
+    nums = numpy.random.normal(mu, sigma,len(indexs))
+    ans_vec=[]
+    for i in range(len(indexs)):
+        num=int(nums[i])
+        ans_vec.append(url_t(url_vec[indexs[i]],num))
+        for j in range(num):
+            f.write(url_vec[indexs[i]]+'\n')
+    ans_vec.sort()
+
+    for ele in ans_vec:
+        fa.write(ele.url+" "+str(ele.num)+'\n')
+    f.close()
+    fa.close()
+
+#main
+gen_from_ans()
+#read in urls
+# try:
+#     f=open('../test/urls.txt','r')
+# except Exception as e:
+#     print(e)
+#
+# url_vec=[]
+# for line in f:
+#     line=line.strip()
+#     url_vec.append(line)
+# f.close()
+#
+# gen_normal_distributed(mu,sigma,urls_needed)
+
